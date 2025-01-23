8d563c9d744e84a322db3d94f34f8eab6b022790
nikkoz0 <160867433+nikkoz0@users.noreply.github.com>
2025-01-23 21:55:43 +0500

    to work: settings, Images, work with cords, better statistics

---
+++ draw_table.py
@@ -0,0 +1,22 @@
+import pygame
+
+from Base_func import WIDTH, HEIGHT
+
+
+def draw_table(results, scroll, screen):
+    results = sorted(results, key=lambda x: x[0], reverse=True)
+    font = pygame.font.Font(None, 36)
+    header = font.render('Пройденный путь       Дата забега', True, (255, 255, 255))
+    screen.blit(header, (WIDTH // 2 - WIDTH//2.95, HEIGHT // 20))
+    c = 1
+    for i in range(scroll, len(results)):
+        y = 30 * (c + 1)
+        if y > HEIGHT // 2 + HEIGHT//5:
+            break
+        dist = results[i][0]
+        date = results[i][1].split('.')[0]
+        text = f'{dist: < 20} {date}'
+        surf = font.render(text, True, (0, 100, 124))
+        screen.blit(surf, (WIDTH // 2 - WIDTH//4, y))
+        c += 1
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
+import sqlite3
 
 from Base_func import WIDTH, HEIGHT, terminate, load_image, FPS
 from ButtonClass import Button
+from draw_table import draw_table
 
 
 pygame.init()
@@ -13,15 +15,15 @@ main_img = load_image('main_menu.png')
 def main():
     button_group = pygame.sprite.Group()
 
-    start_btn = Button(WIDTH // 2 - 160, HEIGHT // 2 - 240, 339, 92, '', 'button.png',
+    start_btn = Button(WIDTH // 2 - 169.5, HEIGHT // 2 - HEIGHT // 3, 339, 92, 'Играть', 'button.png',
                        'button_clicked.png', 'data/click.mp3', button_group)
 
-    settings_btn = Button(WIDTH // 2 - 160, HEIGHT // 2 - 110, 339, 92, '', 'button.png',
+    settings_btn = Button(WIDTH // 2 - 169.5, HEIGHT // 2 - HEIGHT // 6.5, 339, 92, 'Настройки', 'button.png',
                           'button_clicked.png', 'data/click.mp3', button_group)
 
-    choose_chr_btn = Button(WIDTH // 2 - 160, HEIGHT // 2 + 20, 339, 92, '', 'button.png',
+    score_btn = Button(WIDTH // 2 - 169.5, HEIGHT // 2 + HEIGHT // 33, 339, 92, 'Статистика', 'button.png',
                             'button_clicked.png', 'data/click.mp3', button_group)
-    exit_button = Button(WIDTH // 2 - 160, HEIGHT // 2 + 150, 339, 92, '', 'button.png',
+    exit_button = Button(WIDTH // 2 - 169.5, HEIGHT // 2 + HEIGHT//5, 339, 92, 'Выйти', 'button.png',
                          'button_clicked.png', 'data/click.mp3', button_group)
 
     running = True
@@ -35,8 +37,8 @@ def main():
                 return# будет вызов функции самой игры
             if settings_btn.clicked(event):
                 settings()
-            if choose_chr_btn.clicked(event):
-                choose_chr()
+            if score_btn.clicked(event):
+                score()
 
         screen.fill(pygame.Color('black'))
         screen.blit(main_img, (0, 0))
@@ -51,13 +53,9 @@ def main():
 def settings():
     button_group = pygame.sprite.Group()
 
-
-    exit_button = Button(WIDTH // 2 - 160, HEIGHT // 2 + 150, 339, 92, '', 'button.png',
+    exit_button = Button(WIDTH // 2 - 169.5, HEIGHT // 2 + HEIGHT//5, 339, 92, 'Назад', 'button.png',
                          'button_clicked.png', 'data/click.mp3', button_group)
     running = True
-
-    overlay = pygame.Surface((WIDTH, HEIGHT))
-    overlay.fill((0, 0, 0))
 
     while running:
         for event in pygame.event.get():
@@ -78,8 +76,47 @@ def settings():
         pygame.display.flip()
 
 
-def choose_chr():
-    pass
+def score():
+    button_group = pygame.sprite.Group()
+
+    exit_button = Button(WIDTH // 2 - 169.5, HEIGHT // 2 + HEIGHT//5, 339, 92, 'Назад', 'button.png',
+                         'button_clicked.png', 'data/click.mp3', button_group)
+
+    con = sqlite3.connect('data/stat.db')
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM stat')
+    scores = cursor.fetchall()
+    con.close()
+    scroll = 0
+    row = 30
+
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                terminate()
+
+            if exit_button.clicked(event):
+                return
+            if event.type == pygame.MOUSEWHEEL:
+                if event.y == 1:
+                    scroll = max(scroll - 1, 0)
+                elif event.y == -1:
+                    max_down = len(scores) * row
+                    scroll = min(scroll + 1, max_down)
+        screen.fill(pygame.Color('black'))
+        screen.blit(main_img, (-600, 0))
+        button_group.update(pygame.mouse.get_pos())
+
+        for button in button_group:
+            button.draw(screen)
+
+        draw_table(scores, scroll, screen)
+        pygame.display.flip()
+        clock.tick(FPS)
 
 
 if __name__ == '__main__':
